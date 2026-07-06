2357c2cb773eecada758c876511b06bdc4936fb8
Cocobio <iosorio@udec.cl>
2026-07-05 23:51:55 -0400

    Pequeño refacto

--- hello_world/src/helloworld.c
+++ hello_world/src/helloworld.c
@@ -39,113 +39,43 @@ u8 TxBuffer0[BUFFER_SIZE_1] __attribute__ ((aligned(32)));
 u8 TxBuffer1[BUFFER_SIZE_2] __attribute__ ((aligned(32)));
 static u32 RxBuffer[RX_WORDS] __attribute__ ((aligned(32)));
 
-
-int init_sd_and_read_files() {
-    UINT bytesRead;
-
-    // 1. Montar SD
-    res = f_mount(&fatfs, "0:", 1);
-    if (res != FR_OK) return XST_FAILURE;
-
-    // 2. Abrir archivos
-    if (f_open(&fil0, "31mers_5.txt", FA_READ) != FR_OK) return XST_FAILURE;
-    if (f_open(&fil1, "31mers_6.txt", FA_READ) != FR_OK) return XST_FAILURE;
-
-    // 3. Leer y aplicar Padding
-    for (int i = 0; i < NUM_KMERS_1; i++) {
-        // Leer 31 bytes del archivo 0
-        f_read(&fil0, &TxBuffer0[i * KMER_TOTAL_BYTES], KMER_SIZE_BYTES, &bytesRead);
-        char dummy;
-        f_read(&fil0, &dummy, 1, &bytesRead);
-        TxBuffer0[(i * KMER_TOTAL_BYTES) + 31] = 0x00; // Padding
-    }
-    for (int i = 0; i < NUM_KMERS_2; i++) {
-        // Leer 31 bytes del archivo 1
-        f_read(&fil1, &TxBuffer1[i * KMER_TOTAL_BYTES], KMER_SIZE_BYTES, &bytesRead);
-        char dummy;
-        f_read(&fil1, &dummy,1, &bytesRead);
-        TxBuffer1[(i * KMER_TOTAL_BYTES) + 31] = 0x00; // Padding
-    }
-
-    xil_printf("Archivos leidos.\n");
-    f_close(&fil0);
-    f_close(&fil1);
-
-    return XST_SUCCESS;
-}
-
-typedef struct {
-    unsigned char data[31];
-    unsigned char dummy;
-} kmer;
-
-
+int init_sd();
+int read_kmer_files();
+int init_axidma(XAxiDma* axi_dma, XAxiDma_Config *cfg_ptr, unsigned DMA_ID, char id);
+int write_histogram_file(unsigned* histogram, size_t n);
 size_t get_max_stride(unsigned* a, size_t n);
 void count_strides(unsigned* a, size_t n, unsigned* stride_count);
 unsigned sw_validation();
-int write_histogram_file(unsigned* histogram, size_t n);
 
 int main() {
     XTime t_start0, t_start1, t_end;
     unsigned long long u_time0, u_time1, u_time2;
-
-    xil_printf("Hello world.\r\n");
+	int status;
 
     init_platform();
-
-    if (init_sd_and_read_files() != XST_SUCCESS) {
+    if (init_sd() != XST_SUCCESS) {
+        xil_printf("No se pudo montar la uSD.");
+        return 1;
+    }
+    if (read_kmer_files() != XST_SUCCESS) {
         xil_printf("Error al leer los archivos\r\n");
         return 1;
     }
 
-////////////////////////////////////////////////////////////////////////////////
+    // Start PL timer con sus configs
     XTime_GetTime(&t_start0);
 
     XAxiDma_Config *CfgPtr0; 
     XAxiDma_Config *CfgPtr1; 
 
-	int status;
-
-	CfgPtr0 = XAxiDma_LookupConfig(DMA_0_ID);
-	if (!CfgPtr0) {
-		xil_printf("No config found for DMA0\r\n");
-		return XST_FAILURE;
-	}
-	CfgPtr1 = XAxiDma_LookupConfig(DMA_1_ID);
-	if (!CfgPtr1) {
-		xil_printf("No config found for DMA1\r\n");
-		return XST_FAILURE;
-	}
-
-	status = XAxiDma_CfgInitialize(&AxiDma0, CfgPtr0);
-	if (status != XST_SUCCESS) {
-		xil_printf("DMA0 initialization failed %d\r\n", status);
-		return XST_FAILURE;
-	}
-	status = XAxiDma_CfgInitialize(&AxiDma1, CfgPtr1);
-	if (status != XST_SUCCESS) {
-		xil_printf("DMA1 initialization failed %d\r\n", status);
-		return XST_FAILURE;
-	}
-
-	if(XAxiDma_HasSg(&AxiDma0)){
-		xil_printf("Device configured as SG mode \r\n");
-		return XST_FAILURE;
-	}
-	if(XAxiDma_HasSg(&AxiDma1)){
-		xil_printf("Device configured as SG mode \r\n");
-		return XST_FAILURE;
-	}
-
-	XAxiDma_IntrDisable(&AxiDma0, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DEVICE_TO_DMA);
-	XAxiDma_IntrDisable(&AxiDma0, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DMA_TO_DEVICE);
-	XAxiDma_IntrDisable(&AxiDma1, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DMA_TO_DEVICE);
-////////////////////////////////////////////////////////////////////////////////
+    init_axidma(&AxiDma0, CfgPtr0, DMA_0_ID, '0');
+    init_axidma(&AxiDma1, CfgPtr1, DMA_1_ID, '1');
 
     Xil_DCacheFlushRange((UINTPTR)TxBuffer0, TX_BYTES);
     Xil_DCacheFlushRange((UINTPTR)TxBuffer1, TX_BYTES);
     Xil_DCacheFlushRange((UINTPTR)RxBuffer, RX_BYTES);
 
+    // Start PL timer con transferencias + procesamiento de los datos
     XTime_GetTime(&t_start1);
 
     status = XAxiDma_SimpleTransfer(&AxiDma0, (UINTPTR)RxBuffer, RX_BYTES, XAXIDMA_DEVICE_TO_DMA);
@@ -168,8 +98,7 @@ int main() {
 
     while (XAxiDma_Busy(&AxiDma0, XAXIDMA_DMA_TO_DEVICE) ||
            XAxiDma_Busy(&AxiDma1, XAXIDMA_DMA_TO_DEVICE) ||
-           XAxiDma_Busy(&AxiDma0, XAXIDMA_DEVICE_TO_DMA)) {
-    }
+           XAxiDma_Busy(&AxiDma0, XAXIDMA_DEVICE_TO_DMA));
 
     Xil_DCacheInvalidateRange((UINTPTR)RxBuffer, RX_BYTES);
 
@@ -177,19 +106,22 @@ int main() {
     // Polling in_status hasta que sea 1
     while(!Xil_In32(XPAR_AXIREGS_0_BASEADDR+4));
     unsigned matchers = Xil_In32(XPAR_AXIREGS_0_BASEADDR);
-    XTime_GetTime(&t_end);
-    u_time0 = (t_end-t_start0)*1000000/COUNTS_PER_SECOND;
-    u_time1 = (t_end-t_start1)*1000000/COUNTS_PER_SECOND;
 
+    // Stop PL timer
+    XTime_GetTime(&t_end);
+    u_time0 = (t_end-t_start0)*1000000/COUNTS_PER_SECOND; // configs
+    u_time1 = (t_end-t_start1)*1000000/COUNTS_PER_SECOND; // solo trans+process
+
+    // Creacion del histograma
     unsigned* compared_kmers = (unsigned*)RxBuffer;
     size_t compared_kmers_size = RX_WORDS;
 
-    // Create histogram
     size_t hist_size = get_max_stride(compared_kmers, compared_kmers_size) + 1;
     unsigned* histogram = (unsigned*)calloc(hist_size, sizeof(unsigned));
     count_strides(compared_kmers, compared_kmers_size, histogram);
 
     // Print results
+    xil_printf("Strides:\r\n");
     for (size_t i=0; i<hist_size; ++i) {
         if (histogram[i])
             xil_printf("Length %u = Count %u\r\n", i, histogram[i]);
@@ -201,12 +133,15 @@ int main() {
     XTime_GetTime(&t_end);
     u_time2 = (t_end-t_start0)*1000000/COUNTS_PER_SECOND;
     ////////////////
+
+    // Print de resultados finales
+    xil_printf("===============================\r\n");
     xil_printf("Number of matches from PL: %u\r\n", matchers);
     xil_printf("Number of matches from sw: %u\r\n", sw_matches);
-
     xil_printf("PL time =             %llu us\r\n", u_time0);
     xil_printf("PL time (no config) = %llu us\r\n", u_time1);
     xil_printf("PS time =             %llu us\r\n", u_time2);
+    xil_printf("===============================\r\n");
 
     // Save histogram
     if(write_histogram_file(histogram, hist_size) != XST_SUCCESS) {
@@ -281,7 +216,7 @@ int write_histogram_file(unsigned* histogram, size_t n) {
     // 1. Abrir archivos
     if (f_open(&fil2, "hist.txt", FA_CREATE_ALWAYS | FA_WRITE) != FR_OK) return XST_FAILURE;
 
-    // 2. Leer y aplicar Padding
+    // 2. Escribir todos los bins y sus counts
     char s[10];
     char space = ' ', endl = '\n', colon = ':';
     for (size_t i=0, j; i<n; ++i) {
@@ -299,8 +234,69 @@ int write_histogram_file(unsigned* histogram, size_t n) {
         f_write(&fil2, &endl, 1, &bytesWritten);
     }
 
-    xil_printf("Archivo escrito.\r\n");
+    xil_printf("Archivo del histograma escrito.\r\n");
     f_close(&fil2);
+
+    return XST_SUCCESS;
+}
+
+
+int init_sd() {
+    res = f_mount(&fatfs, "0:", 1);
+    if (res != FR_OK) return XST_FAILURE;
+    return XST_SUCCESS;
+}
+
+
+int read_kmer_files() {
+    UINT bytesRead;
+    // 1. Abrir archivos
+    if (f_open(&fil0, "31mers_5.txt", FA_READ) != FR_OK) return XST_FAILURE;
+    if (f_open(&fil1, "31mers_6.txt", FA_READ) != FR_OK) return XST_FAILURE;
+
+    // 2. Leer y aplicar Padding
+    for (int i = 0; i < NUM_KMERS_1; i++) {
+        // Leer 31 bytes del archivo 0
+        f_read(&fil0, &TxBuffer0[i * KMER_TOTAL_BYTES], KMER_TOTAL_BYTES, &bytesRead);
+        TxBuffer0[(i * KMER_TOTAL_BYTES) + 31] = 0x00; // Padding
+    }
+    for (int i = 0; i < NUM_KMERS_2; i++) {
+        // Leer 31 bytes del archivo 1
+        f_read(&fil1, &TxBuffer1[i * KMER_TOTAL_BYTES], KMER_TOTAL_BYTES, &bytesRead);
+        TxBuffer1[(i * KMER_TOTAL_BYTES) + 31] = 0x00; // Padding
+    }
+
+    xil_printf("Archivos leidos.\n");
+    f_close(&fil0);
+    f_close(&fil1);
+
+    return XST_SUCCESS;
+}
+
+
+int init_axidma(XAxiDma* axi_dma, XAxiDma_Config *cfg_ptr, unsigned DMA_ID, char id) {
+
+	int status;
+
+	cfg_ptr = XAxiDma_LookupConfig(DMA_ID);
+	if (!cfg_ptr) {
+		xil_printf("No config found for DMA%c\r\n", id);
+		return XST_FAILURE;
+	}
+
+	status = XAxiDma_CfgInitialize(axi_dma, cfg_ptr);
+	if (status != XST_SUCCESS) {
+		xil_printf("DMA%c initialization failed %d\r\n", id, status);
+		return XST_FAILURE;
+	}
+
+	if(XAxiDma_HasSg(axi_dma)){
+		xil_printf("Device configured as SG mode \r\n");
+		return XST_FAILURE;
+	}
+
+	XAxiDma_IntrDisable(axi_dma, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DEVICE_TO_DMA);
+	XAxiDma_IntrDisable(axi_dma, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DMA_TO_DEVICE);
 
     return XST_SUCCESS;
 }
